af2c94dc2225ff0efee997bdcbca8ac113bad84b
aal80 <anton.al80@gmail.com>
2026-05-15 15:46:10 -0500

    deleted gradio dashboard

--- workshops/agentcore-workday-integration/dashboard-gradio/logging_config.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-def configure_logging():
-    logging.basicConfig(level=logging.INFO)
-    logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
-    logging.getLogger("httpx").setLevel(logging.WARNING)
