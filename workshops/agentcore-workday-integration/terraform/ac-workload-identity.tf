2cbbd4ed55c84f0cc578d3bd6f2fa514fc0d6428
aal80 <anton.al80@gmail.com>
2026-05-25 18:37:19 -0500

    Update ac-workload-identity.tf

--- workshops/agentcore-workday-integration/terraform/ac-workload-identity.tf
+++ workshops/agentcore-workday-integration/terraform/ac-workload-identity.tf
@@ -12,28 +12,28 @@ resource "local_file" "workload_identity_name" {
 # }
 
 
-resource "aws_cloudwatch_log_group" "wlid" {
-  name              = "/aws/bedrock-agentcore/wlid"
-  retention_in_days = 7
-}
+# resource "aws_cloudwatch_log_group" "wlid" {
+#   name              = "/aws/bedrock-agentcore/wlid"
+#   retention_in_days = 7
+# }
 
-resource "aws_cloudwatch_log_delivery_source" "wlid" {
-  name         = "wlid"
-  log_type     = "APPLICATION_LOGS"
-  resource_arn = aws_bedrockagentcore_workload_identity.hr_agent.workload_identity_arn
-}
+# resource "aws_cloudwatch_log_delivery_source" "wlid" {
+#   name         = "wlid"
+#   log_type     = "APPLICATION_LOGS"
+#   resource_arn = aws_bedrockagentcore_workload_identity.hr_agent.workload_identity_arn
+# }
 
-resource "aws_cloudwatch_log_delivery_destination" "wlid" {
-  name = "wlid"
+# resource "aws_cloudwatch_log_delivery_destination" "wlid" {
+#   name = "wlid"
 
-  delivery_destination_configuration {
-    destination_resource_arn = aws_cloudwatch_log_group.wlid.arn
-  }
+#   delivery_destination_configuration {
+#     destination_resource_arn = aws_cloudwatch_log_group.wlid.arn
+#   }
 
-  output_format = "json"
-}
+#   output_format = "json"
+# }
 
-resource "aws_cloudwatch_log_delivery" "wlid" {
-  delivery_source_name     = aws_cloudwatch_log_delivery_source.wlid.name
-  delivery_destination_arn = aws_cloudwatch_log_delivery_destination.wlid.arn
-}
+# resource "aws_cloudwatch_log_delivery" "wlid" {
+#   delivery_source_name     = aws_cloudwatch_log_delivery_source.wlid.name
+#   delivery_destination_arn = aws_cloudwatch_log_delivery_destination.wlid.arn
+# }
